f76d4cbecfaf835893076ea01883ceafe1c4c709
heurilinares <heurilinares@DESKTOP-7F9G3G4>
2024-02-11 18:55:15 -1200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("POSITIVO O NEGATIVO");
+
+Console.WriteLine();
+
+{
+        Console.WriteLine("Ingrese un número:");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero > 0)
+    {
+        Console.WriteLine("El número ingresado es positivo.");
+    }
+    else if (numero < 0)
+    {
+        Console.WriteLine("El número ingresado es negativo.");
+    }
+    else
+    {
+        Console.WriteLine("El número ingresado es cero.");
+    }
+}
